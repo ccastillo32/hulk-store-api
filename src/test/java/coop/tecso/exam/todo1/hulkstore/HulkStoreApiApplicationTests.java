1146baaf191af6c2b11661cf82ce5be81f7ae8da
ccastillo <cristian.castillo@flexibility.com.ar>
2021-01-04 12:07:52 -0500

    Initial project configuration

---
+++ src/test/java/coop/tecso/exam/todo1/hulkstore/HulkStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package coop.tecso.exam.todo1.hulkstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HulkStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
